b67f59e073d9ed98bc7dcacf95cc645851e823ff
joker1007 <kakyoin.hierophant@gmail.com>
2010-03-27 04:25:42 +0900

    update top_controller & meal_status migration

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,19 +1,32 @@
 class TopController < ApplicationController
-  def index
-    keycode = params[:keycode]
-    @user = User.find_by_keycode(keycode)
+  before_filter :user_check
 
+  def index
     meal_type = Time.now.hour >= 10 || Time.now.hour <= 1 ? "Dinner" : "Breakfast"
     @meal = Meal.find(:first, :conditions => ["date = ? AND meal_type = ?", Date.today, meal_type])
 
-    @eatable = MealStatus.find(:all, :conditions => ["meal_id = ? AND user_id = ?", @meal.id, @user.id]).empty? ? true : false
+    @eatable = MealStatus.eatable?(@user.id, @meal.id)
 
-    if @eatable
+    if @eatable == "eatable"
       @message = "#{@user.name}さんは本日お食事予定です"
-    else
+    elsif @eatable == "rejected"
       @message = "#{@user.name}さんは本日欠食のため食べられません"
+    elsif @eatable == "sold"
+      @message = "#{@user.name}さんは本日緊急欠食のため食べられません"
     end
 
     @meal_statuses = MealStatus.find(:all, :conditions => ["user_id = ? OR matched_user_id IS NOT NULL", @user.id], :limit => 10, :order => "date")
   end
+
+  def view_list
+    meal_type = Time.now.hour >= 10 || Time.now.hour <= 1 ? "Dinner" : "Breakfast"
+    @meal = Meal.find(:first, :conditions => ["date = ? AND meal_type = ?", Date.today, meal_type])
+  end
+
+  private
+  def user_check
+    keycode = params[:keycode]
+    @user = User.find_by_keycode(keycode)
+  end
+
 end
--- app/models/meal_status.rb
+++ app/models/meal_status.rb
@@ -2,4 +2,9 @@ class MealStatus < ActiveRecord::Base
   belongs_to :user
   belongs_to :meal
   belongs_to :matched_user, :class_name => "User"
+
+  def self.eatable?(user_id, meal_id)
+    ms = MealStatus.find(:first, :conditions => ["user_id = ? AND meal_id = ?", user_id, meal_id])
+    ms.nil? ? "eatable" : ms.status
+  end
 end
