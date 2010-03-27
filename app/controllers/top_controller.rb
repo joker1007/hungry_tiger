b8cd0d8e5d520e8a2bbf739aba3a966808f7aacb
Kotaro Miyake <pggm0@hotmail.com>
2010-03-27 16:03:50 +0900

    休職申請感性

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -40,22 +40,37 @@ class TopController < ApplicationController
     meals.each do |key, value|
       meal = Meal.find(key)
       meal_status_tmp = @user.meal_statuses.find(:first, :conditions => { :meal_id => meal.id})
-      if meal_status_tmp
-        #すでに同一のuser_idとmeal_idを持つmeal_statusesが存在する場合、更新を行う
-        meal_status = meal_status_tmp.update_attribute(:status, "rejected")
+
+      if value == "yes"
+      #チェックされた場合
+        if meal_status_tmp
+          #すでに同一のuser_idとmeal_idを持つmeal_statusesが存在する場合、何もしない
+          #meal_status = meal_status_tmp.update_attribute(:status, "rejected")
+        else
+          #同一のuser_idとmeal_idを持つmeal_statusesが存在しない場合
+          if  @user.meal_statuses.create(:meal_id => meal.id, :date => meal.date, :meal_type => meal.meal_type, :status => "rejected")
+            notice += "#{meal.date.strftime("%m/%d")} #{meal.name}を欠食にしました<br>"
+          else
+            flash[:error] = "欠食申請が失敗しました"
+            redirect_to :controller => "top", :action => "menu", :keycode => @user.keycode
+          end
+        end  
+        
       else
-        #同一のuser_idとmeal_idを持つmeal_statusesが存在しない場合
-        meal_status = @user.meal_statuses.create(:meal_id => meal.id, :date => meal.date, :meal_type => meal.meal_type, :status => "rejected")
-      end
-      
-      if meal_status
-        notice += "#{meal.date.strftime("%m/%d")} #{meal.name}を欠食にしました<br>"
-      else
-        flash[:error] = "欠食申請が失敗しました"
-        redirect_to :controller => "top", :action => "menu", :keycode => @user.keycode
+      #チェックしてない場合
+        if meal_status_tmp
+          #同じのがあったら、消す
+          if meal_status_tmp.destroy
+            notice += "#{meal.date.strftime("%m/%d")} #{meal.name}の欠食を解除しました<br>"
+          else
+            flash[:error] = "欠食申請が失敗しました"
+            redirect_to :controller => "top", :action => "menu", :keycode => @user.keycode
+          end
+        else
+          #同じのがなかったら、そのまま
+        end
       end
     end
-    
     flash[:notice] = notice
     redirect_to :controller => "top", :action => "menu", :keycode => @user.keycode
   end
