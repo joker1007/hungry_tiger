61e0308768272d59b699b14aebb18b4b5ec2a304
joker1007 <kakyoin.hierophant@gmail.com>
2010-03-27 10:51:46 +0900

    top/sell

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   def reject_apply_link(user_id, meal_id)
-    if MealStatus.eatable?(user_id, meal_id)
+    if MealStatus.eatable?(user_id, meal_id) == "eatable"
       link_to("欠食申請", :controller => "apply", :action => "reject_confirm", :keycode => @user.keycode, :meal_id => meal_id)
     else
       link_to("欠食取消", :controller => "apply", :action => "reject_cancel", :keycode => @user.keycode, :meal_id => meal_id)
@@ -9,10 +9,10 @@ module ApplicationHelper
   end
 
   def sell_apply_link(user_id, meal_id)
-    if MealStatus.eatable?(user_id, meal_id)
+    if MealStatus.eatable?(user_id, meal_id) == "eatable"
       link_to("緊急欠食申請", :controller => "apply", :action => "sell_confirm", :keycode => @user.keycode, :meal_id => meal_id)
     else
-      link_to("余り", :controller => "apply", :action => "buy_confirm", :keycode => @user.keycode, :meal_id => meal_id)
+      link_to("やっぱり食べたい", :controller => "apply", :action => "buy_confirm", :keycode => @user.keycode, :meal_id => meal_id)
     end
   end
 end
